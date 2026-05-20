3befad08fc792f0c0ce5dc018061ee4441bd41c8
aurorebilley <aurorebilleybanry@gmail.com>
2026-05-20 14:44:21 +0200

    Description des changements

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -209,7 +209,7 @@ export default function HomePage() {
           .home-dancer { top: 45%; width: min(72vw, 340px); max-height: 62vh; }
           .home-boutique-button {
             top: auto;
-            bottom: 168px;
+            bottom: 204px;
             width: min(58vw, 230px);
           }
           .home-left-buttons {
