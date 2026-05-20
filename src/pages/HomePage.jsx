0a6a40aea98e9a03b8e7f749676a107a5a8906e7
aurorebilley <aurorebilleybanry@gmail.com>
2026-05-20 14:47:18 +0200

    Description des changements

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -132,7 +132,7 @@ export default function HomePage() {
           left: 50%;
           top: calc(50% + min(22vw, 250px));
           z-index: 8;
-          width: min(36vw, 390px);
+          width: min(44vw, 500px);
           padding: 0;
           border: 0;
           background: transparent;
